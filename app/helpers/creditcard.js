de582aea525c9f53ae57b80a4bf6e91dee774a81
Steven Ray <mail@stevenray.com>
2015-03-08 14:17:31 -0500

    Added interface for inputs
    Added sample interface of credit card inputs for dev

---
+++ app/helpers/creditcard.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+
+
+// isAPR: function(){
+//   return this.get()
+// }
+
+
+
+
+
+
+/**
+isBalance - FIELD
+isAPR - FIELD
+isDailyPercentageRate - notshown
+isDaysInMonth - notshown
+isInterestForMonth - notshown (isBalance * isDailyPercentageRate * isDaysInMonth)
+isBalancePlusInterest - notshown
+isMinimumPayment - FIELD
+isBalanceAfterPayment - notshown
+isPaymentToPriniciple - notshown
+isPaymentToInterest - DISPLAYED maybe
+isMonthsUntilZero - DISPLAYED
+
+
+-----
+Avg daily balance. is days x same balance added together and divided by total days.
+Percentage 14 is .14, is divided by 365 (days per year)
+
+Monthly payments = (Principal)(APR/1200 + (APR/1200)/((1+APR/1200)^Term -1))
+AVG monthly payment is
+
+**/
+
