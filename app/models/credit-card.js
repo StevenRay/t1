6ff46442cc6656e179aa2e28dfae7e5198986c2b
Steven Ray <mail@stevenray.com>
2015-03-10 00:01:29 -0500

    Solved the concatenation of numbers issue
    Solved the concatenation of numbers issue by adding a -1+1. Seems like
    a hack but I’ll take it for now.

--- app/models/credit-card.js
+++ app/models/credit-card.js
@@ -5,16 +5,17 @@ export default DS.Model.extend({
   apr: DS.attr('number'),
 
   interestDaily: function(){
-    return (this.get('apr')/100)/365;
+    return (this.get('apr')*0.01)/365;
   }.property('apr'),
 
   interestThisPeriod: function(){
     return this.get('balance')*this.get('interestDaily')*31/**days in month**/;
-  }.property('interestDaily'),
+  }.property('balance','interestDaily'),
 
 /** PROBLEM: Concatnating string instead of numbers**/
+// fixed by adding "-1+1", seems like there should be a more elegant way to fix this.
   balanceWithInterest: function(){
-    return this.get('balance')+this.get('interestThisPeriod');
+    return (this.get('balance')-1)+1+(this.get('interestThisPeriod')-1+1);
   }.property('balance','interestThisPeriod'),
 
   payment: DS.attr('number'),
@@ -23,15 +24,17 @@ export default DS.Model.extend({
     return this.get('balanceWithInterest')-this.get('payment');
   }.property('balanceWithInterest', 'payment'),
 
+ paidToPrinciple: function(){
+    return this.get('payment')-this.get('interestThisPeriod');
+  }.property('payment', 'interestThisPeriod'),
 
 
-
-  monthsToZero: function(){
-    //temporary
-    // Need to ad loop to get months
-    // Need to add && so that it requires both to return monthsToZero
-    return (this.get('balance')-this.get('payment'));
-  }.property('balance','payment')
+  // monthsToZero: function(){
+  //   //temporary
+  //   // Need to ad loop to get months
+  //   // Need to add && so that it requires both to return monthsToZero
+  //   return (this.get('balance')-this.get('payment'));
+  // }.property('balance','payment')
 
 
 /**   return (this.get('balance')*(((this.get('apr')/100)/365)*this.get('daysInMonth')))+this.get('balance')-this.get('payment');
