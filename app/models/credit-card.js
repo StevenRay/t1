69fd4e65cc7bb028a95837325f8e618a00511057
Steven Ray <mail@stevenray.com>
2015-03-10 10:30:20 -0500

    Refactored syntax & rounded to 2 decimals.
    Fixed the -1+1 syntax issue on the ‘balanceWithInterest’, rounded all
    numbers to 2 decimals with “.toFixed(2)”.

--- app/models/credit-card.js
+++ app/models/credit-card.js
@@ -1,46 +1,38 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
-  balance: DS.attr('number'),
-  apr: DS.attr('number'),
+  balance: DS.attr('number', {defaultValue: 0}),
+  apr: DS.attr('number', {defaultValue: 0}),
+  payment: DS.attr('number', {defaultValue: 0}),
 
   interestDaily: function(){
     return (this.get('apr')*0.01)/365;
   }.property('apr'),
 
   interestThisPeriod: function(){
-    return this.get('balance')*this.get('interestDaily')*31/**days in month**/;
+    return this.get('balance') * (this.get('interestDaily') * 31) /**days in month**/;
   }.property('balance','interestDaily'),
 
-/** PROBLEM: Concatnating string instead of numbers**/
-// fixed by adding "-1+1", seems like there should be a more elegant way to fix this.
   balanceWithInterest: function(){
-    return (this.get('balance')-1)+1+(this.get('interestThisPeriod')-1+1);
+    return (Number(this.get('balance')) + this.get('interestThisPeriod')).toFixed(2);
   }.property('balance','interestThisPeriod'),
 
-  payment: DS.attr('number'),
-
   balanceAfterPayment: function(){
-    return this.get('balanceWithInterest')-this.get('payment');
+    return this.get('balanceWithInterest')-this.get('payment').toFixed(2);
   }.property('balanceWithInterest', 'payment'),
 
  paidToPrinciple: function(){
-    return this.get('payment')-this.get('interestThisPeriod');
+    return this.get('payment') - this.get('interestThisPeriod').toFixed(2);
   }.property('payment', 'interestThisPeriod'),
 
 
   // monthsToZero: function(){
-  //   //temporary
-  //   // Need to ad loop to get months
-  //   // Need to add && so that it requires both to return monthsToZero
-  //   return (this.get('balance')-this.get('payment'));
-  // }.property('balance','payment')
-
-
-/**   return (this.get('balance')*(((this.get('apr')/100)/365)*this.get('daysInMonth')))+this.get('balance')-this.get('payment');
-      }.property('balance','payment','apr','payment')
-**/
-});
+  //   for(this.get('balance')=this.get('balanceAfterPayment'); this.get('balance')>0; this.get('balance')-this.get('payment')){
+  //     console.log('balance');
+  //     //return this.get('');
+  //   }
+  });
 
 /**
 Balance - FIELD
